79901b81b5008175b789cb8ef99c692d770ca6aa
Colette Montminy <cmontminy@utexas.edu>
2020-04-02 05:05:32 -0500

    Updates

---
+++ precache-manifest.1142cebc3b932d9cd29ef50ca622791b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88165d7c46cb0cebc39e012be5e396b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "592d1c34b685d30f8fab",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90ea822fdf30edfa2cee",
+    "url": "/static/js/2.128261ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.128261ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592d1c34b685d30f8fab",
+    "url": "/static/js/main.db917b86.chunk.js"
+  },
+  {
+    "revision": "81786c6fd50766777295",
+    "url": "/static/js/runtime-main.780b3817.js"
+  }
+]);
